3967157982a8d02e3b6a3eb3547bdf3f2538cc91
EyshilaNuness <93560193+Eyshilasara@users.noreply.github.com>
2023-11-24 10:05:04 -0300

    locacaomidia cliente.cpp
    locacao midia

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -1,28 +1,27 @@
 #include "cliente.hpp"
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <ctime>
 
-Cliente::Cliente(const std::string& nome, const std::string& cpf) {
-    this->nome = nome;
-    this->cpf = cpf;
-}
-
-void Cliente::locacaoMidia(std::map<std::string, std::pair<std::string, int>>& midias, const std::string& codigo) {
-    auto it = midias.find(codigo);
-    if (it != midias.end() && it->second.second > 0) {
-        it->second.second--;
-        registrarLocacao(codigo);
-    } else {
-        std::cout << "Não há quantidade disponível de " << it->second.first << " para locação" << std::endl;
-    }
-}
-
-void Cliente::registrarLocacao(const std::string& codigo) {
+void Cliente::locacaoMidia(std::map<std::string, std::pair<std::string, int>>& midias, const std::vector<std::string>& codigos) {
     std::ofstream arquivo;
     arquivo.open("locacoes.txt", std::ios::app);
+
     time_t agora = time(0);
     tm* ltm = localtime(&agora);
-    arquivo << "0," << 1900 + ltm->tm_year << '-' << 1 + ltm->tm_mon << '-' << ltm->tm_mday << ',' << cpf << ',' << codigo << std::endl;
+    std::string dataLocacao = std::to_string(1900 + ltm->tm_year) + '-' + std::to_string(1 + ltm->tm_mon) + '-' + std::to_string(ltm->tm_mday);
+
+    for (const std::string& codigo : codigos) {
+        auto it = midias.find(codigo);
+
+        if (it != midias.end() && it->second.second > 0) {
+            it->second.second--;
+            arquivo << "Status: 0, " << "Data: " << dataLocacao << ',' << "CPF do cliente: " << this->cpf << ',' << "Código da mídia: " << codigo << std::endl;
+        } else {
+            std::cout << "Não há quantidade disponível de " << it->second.first << " para locação" << std::endl;
+        }
+    }
+
     arquivo.close();
 }

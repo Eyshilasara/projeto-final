cf276c09b4936099882ea760563f40c07dc6a45c
André <anpegustavo@gmail.com>
2023-12-01 14:31:18 -0300

    Modelo main

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(){
             bool teste = banco.Pesquisar("307");
             cout << teste;
         }
-        else if(entrada == "IR"){
+        else if(entrada == "LC"){
             do{
             cout << "Digite C para ordenar de acordo com o CPF ou N para ordem alfabética dos nomes."<< endl;
             cin >> ordem;
@@ -37,6 +37,40 @@ int main(){
             banco.imprimirRelatorio(ordem);
             
         }
+
+        else if(entrada == "LF"){
+            do{
+            cout << "Digite C para ordenar de acordo com o código ou T para ordem alfabética dos títulos."<< endl;
+            cin >> ordem;
+            if(ordem != 'C' && ordem != 'T')
+                cout << "Entrada inválida."<< endl;
+            
+            }while(ordem != 'C' && ordem != 'T');
+
+        }
+
+        else if(entrada == "LA"){
+
+        }
+        else if(entrada == "CF"){
+
+        }
+        else if(entrada == "RF"){
+
+        }
+        else if(entrada == "LF"){
+
+        }
+
+        else if(entrada == "AL"){
+
+        }
+        else if(entrada == "DV"){
+        }
+
+
+        else if(entrada == "FS")
+            break;
 
         
 

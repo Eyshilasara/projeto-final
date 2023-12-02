d893452f3088c02f1313f5cc87c0a365caffa95a
lehsc <150151818+lehsc@users.noreply.github.com>
2023-12-02 10:21:40 -0300

    Update Cliente.cpp

--- devolucao_midia/Cliente.cpp
+++ devolucao_midia/Cliente.cpp
@@ -8,93 +8,107 @@
 #include <vector>
 #include <sstream>
 #include <random>
-#include "File_Manager.hpp"
-#include "Rent.hpp"
-#include "File_Manager.hpp"
+#include "Gerenciador_de_arquivos.hpp"
+
 using namespace std;
 using namespace chrono;
 
 
 Cliente::Cliente(const string& nome, long long cpf) : Pessoa(nome, cpf) {}
 
-
-Rent find(vector<Rent> rentList, long long cpf) {
-	size_t rentalsCount = rentList.size();
-	for (int i = 0; i < rentalsCount; i++) {
-		if (rentList[i].cpf == cpf) {
-			return rentList[i];
-		}
-	}
-
-	return Rent{};
-
+void Cliente::locacaoMidia(vector<string> codigos) {
+  vector<vector<string>> locacoes = PegarLinhasDoArquivo("locacoes.txt");
+  vector<string> locacao;
+  string dataAtual;
+  
+    for (const auto& codigo : codigos) {
+      //verifica e atualiza a disponível para locacao para a mídia específicada pelo codigo
+      if(estoque("midias.txt", codigo, 0) == true){
+        //se deu tudo certo no estoque, atualizar arquivo de locacoes
+        dataAtual = getCurrentDateAsString();
+        locacao = {"0", to_string(this->cpf), codigo, dataAtual.replace(0, 1, ""), "0"};
+        locacoes.push_back(locacao);
+      }
+    }
+    //cria um novo arquivo locacoes com as novas locacoes
+    novoArquivo("locacoes.txt", locacoes);
 }
 
-void Cliente::Devolver_Midia(vector<string> codes) {
-	long long total_value = 0, value = 0;
-	vector<Rent>  rents = getRents();
-	Rent currentCustomer = find(rents, cpf);
-	if (currentCustomer.cpf) {
-		changeRentRowData("locacoes.txt", codes);
-		for (const auto& code : codes) {
-			value = Calcular_Valor(code, currentCustomer.data_locacao);
-			if (!value) {
-				return;
-			}
-			total_value += value;
-		}
+void Cliente::Devolver_Midia(vector<string> codigos) {
+  double valorTotal = 0, valor = 0;
+  
+  string dataAtual = getCurrentDateAsString();
+  cout << dataAtual.replace(0, 1, "") << endl;
+  
+  vector<vector<string>> cliente = filtro("locacoes.txt", 1, to_string(this->cpf), true);
 
-		cout << "Valor total: " << total_value << endl;
-		report(1,to_string(cpf));
-	}
-	else {
-		cerr << "CPF não encontrado";
-	}
+  //verifica se o cliente foi encontrado em alguma locação
+  if (cliente[0][1] == to_string(this->cpf)) {
 
+    //atualiza o status e a data de devoluçao do arq locacoes
+    atualizarDataDeLocacao("locacoes.txt", codigos, cliente[0][1]);
 
+    for (const auto& codigo : codigos) {
+      
+      //atualiza a quantidade disponível do arq mídias para a mídia específicada pelo codigo
+      estoque("midias.txt", codigo, 1);
+
+      valor = Calcular_Valor(codigo, cliente[0][3]);
+
+      if (!valor) {
+        return;
+      }
+      valorTotal += valor;
+    }
+
+    cout << "Valor total: R$ " << valorTotal << endl;
+  }
+  else {
+    cerr << "CPF não encontrado";
+  }
 }
 
-int Cliente::getRentElapsedDays(string rent) {
-	tm tm = {};
-	string date = rent;
-	stringstream ss(rent);
-	ss >> get_time(&tm, "%Y-%m-%d");
-	time_t t = mktime(&tm);
-	system_clock::time_point userTimePoint = system_clock::from_time_t(t);
-	system_clock::time_point currentTimePoint = system_clock::now();
-	chrono::duration_cast<chrono::duration<int>>(currentTimePoint - userTimePoint);
-	auto duration = chrono::duration_cast<chrono::hours>(currentTimePoint - userTimePoint);
-	return duration.count() / 24;
-
+int Cliente::diasCorridos(string aluguel) {
+  tm tm = {};
+  string data = aluguel;
+  stringstream ss(aluguel);
+  ss >> get_time(&tm, "%Y-%m-%d");
+  time_t tempo = mktime(&tm);
+  system_clock::time_point momentoUsuario = system_clock::from_time_t(tempo);
+  system_clock::time_point momentoAtual = system_clock::now();
+  auto duracao = chrono::duration_cast<chrono::hours>(momentoAtual - momentoUsuario);
+  return duracao.count() / 24;
 }
 
-bool isRewindedTape() {
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> distrib(0, 1);
-	return distrib(gen) == 1;
+//gera numero aleatorio entre 0 e 1
+bool fitaEstaRebobinada() {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> distrib(0, 1);
+  return distrib(gen) == 1;
 }
 
+double Cliente::Calcular_Valor(string codigo, string rentDate) {
+  double valor = 0;
+  int dias = diasCorridos(rentDate);
+  string type = verificarTipoMidia(codigo);
 
-int Cliente::Calcular_Valor(string codigo, string rentDate) {
-	int valor = 0, elapsedDays = getRentElapsedDays(rentDate);
+  if (type == "DL") {//DVD’s Lançamento
+    valor = 20 * dias;
+  }
+  else if (type == "DE") {//DVD’s Estoque
+    valor = 10 * dias;
+  }
+  else if (type == "DP") {//DVD’s Promoção
+    valor = 10; //preço fixo
+  }
+  else if (type == "F") {//Fita
+    bool estaRebobinada = fitaEstaRebobinada();
+    valor = estaRebobinada ? 5 + 2 : 5; //preço fixo
+  }
+  else {
+    cerr << "Código não encontrado";
+  }
 
-	if (codigo.substr(0, 2) == "DL") {//DVD’s Lançamento
-		valor = 20 * elapsedDays;
-	}
-	else if (codigo.substr(0, 2) == "DE") {//DVD’s Estoque
-		valor = 10 * elapsedDays;
-	}
-	else if (codigo.substr(0, 2) == "DP") {//DVD’s Promoção
-		valor = 10; //preço fixo
-	}
-	else if (codigo.at(0) == 'F') {//Fita
-		bool isRewinded = isRewindedTape();
-		valor = isRewinded ? 5 + 2 : 5; //preço fixo
-	}
-	else {
-		cerr << "Code not found";
-	}
-
-	return valor;
+  return valor;
 }

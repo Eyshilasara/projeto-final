d3a2c0cc99d4c70901359a4d53413500c69417d9
thali-pires <thalitaf.pires@gmail.com>
2023-12-02 17:06:31 -0300

    alteracoes no arquivo cadastroMidia

--- cadastro_de_midia/cadastroMidia.cpp
+++ cadastro_de_midia/cadastroMidia.cpp
@@ -14,18 +14,34 @@ std::string cadastrarMidia(Midia &midia, DVD &dvd, Disco &disco, Jogo &jogo, Fit
     int quantidadeDisponivel;
     std::string categoria;
     std::string grupo;
-    double notaCritica;
+    int notaCritica;
     std::string plataforma;
     std::string cantorBanda;
     int numeroMusicas;
     bool rebobinado;
 
     std::cout << "Qual o codigo da midia? "
-              << "Use apenas numeros inteiros!" << std::endl;
+              << "O codigo deve conter exatamente 4 digitos. Lembre-se de usar apenas numeros inteiros!" << std::endl;
     std::cin >> codigo;
+    while (true)
+    {
+        std::cin >> codigo;
+        if (std::cin.fail() || std::to_string(codigo).length() != 4)
+        {
+            std::cout << "Codigo invalido. Por favor, insira novamente." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else
+        {
+            break;
+        }
+    }
+
     midia.setCodigo(codigo);
 
     std::cout << "Qual o tipo da mídia? (As opções são dvd ou fita de vídeo. Lembre-se de usar sempre letras minúsculas!)" << std::endl;
+
     while (true)
     {
         std::cin >> tipo;
@@ -43,7 +59,7 @@ std::string cadastrarMidia(Midia &midia, DVD &dvd, Disco &disco, Jogo &jogo, Fit
     }
     midia.setTipo(tipo);
 
-    if (tipo == "DVD")
+    if (tipo == "dvd")
     {
         while (true)
         {
@@ -73,113 +89,189 @@ std::string cadastrarMidia(Midia &midia, DVD &dvd, Disco &disco, Jogo &jogo, Fit
                 std::cout << "Categoria invalida. Por favor, insira novamente." << std::endl;
 
                 std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
             }
         }
         dvd.setCategoria(categoria);
-
-        std::cout << "Qual o titulo da midia? "
-                  << "(Sem caracteres especiais. Lembre-se de usar sempre letras minusculas!)" << std::endl;
+    }
+    std::cout << "Qual o titulo da midia? "
+                  << "(Sem caracteres especiais. Lembre-se de usar sempre letras minusculas e colocar o titulo entre aspas!)" << std::endl;
         std::cin >> titulo;
-        midia.setTitulo(titulo);
 
+        // Verificar se o título contém apenas caracteres alfanuméricos e minúsculas
+        while (true)
+        {
+            if (std::all_of(titulo.begin(), titulo.end(), [](char c)
+                            { return std::islower(c) || std::isalnum(c); }) &&
+                titulo[0] == '"' && titulo.back() == '"')
+            {
+                midia.setTitulo(titulo);
+                break;
+            }
+            else
+            {
+                std::cout << "Título inválido. Por favor, siga as instruções." << std::endl;
+            }
+        }
         std::cout << "Qual o genero da midia? "
-                  << "Lembre-se de usar sempre letras minusculas!)" << std::endl;
-        std::cin >> genero;
-        midia.setGenero(genero);
-
+                  << "Lembre-se de usar sempre letras minusculas e sem caracteres especiais!)" << std::endl;
+        while (true)
+        {
+            if (std::all_of(genero.begin(), genero.end(), [](char c)
+                            { return std::islower(c) || std::isalnum(c); }))
+            {
+                midia.setGenero(genero);
+                break;
+            }
+            else
+            {
+                std::cout << "Genero inválido. Por favor, siga as instruções." << std::endl;
+            }
+        }
         if (grupo == "filme")
         {
             std::cout << "Qual a nota de crítica? (Use apenas numeros inteiros)" << std::endl;
-            if (std::cin >> notaCritica)
+            std::cin >> notaCritica;
+            while (true)
             {
-                filme.setNotaCritica(notaCritica);
+                if (std::cin.fail())
+                {
+                    std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
+                    // Limpar o estado de erro e o buffer de entrada
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
-            {
-                std::cout << "Entrada invalida. Por favor, insira um numero inteiro." << std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
-    }
-    else if (grupo == "jogo")
-    {
-        while (true)
-        {
-            std::cout << "Qual a plataforma do jogo? (As opções são PS4, XBOX ou PC. Lembre-se de usar sempre letras maiusculas!)" << std::endl;
-            std::cin >> plataforma;
-            if (plataforma == "PS4" || plataforma == "XBOX" || plataforma == "PC")
-            {
-                break;
-            }
-            else
-            {
-                std::cout << "Plataforma invalida. Por favor, insira novamente." << std::endl;
-            }
-            jogo.setPlataforma(plataforma);
-        }
-    }
-    else if (grupo == "disco")
-    {
-        std::cout << "Qual o cantor ou banda do disco? (Lembre-se de usar sempre letras minúsculas!)" << std::endl;
-        std::cin >> cantorBanda;
 
-        while (true)
+            filme.setNotaCritica(notaCritica);
+        }
+    
+        else if (grupo == "jogo")
         {
+            while (true)
+            {
+                std::cout << "Qual a plataforma do jogo? (As opções são PS4, XBOX ou PC. Lembre-se de usar sempre letras maiusculas!)" << std::endl;
+                std::cin >> plataforma;
+                if (plataforma == "PS4" || plataforma == "XBOX" || plataforma == "PC")
+                {
+                    break;
+                }
+                else
+                {
+                    std::cout << "Plataforma invalida. Por favor, insira novamente." << std::endl;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+                }
+                jogo.setPlataforma(plataforma);
+            }
+        }
+        else if (grupo == "disco")
+        {
+            std::cout << "Qual o cantor ou banda do disco? (Sem caracteres especiais. Lembre-se de usar sempre letras minusculas e colocar o nome do cantor/banda entre aspas!)" << std::endl;
+            std::cin >> cantorBanda;
+            while (true)
+            {
+                if (std::all_of(cantorBanda.begin(), cantorBanda.end(), [](char c)
+                                { return std::islower(c) || std::isalnum(c); }) &&
+                    titulo[0] == '"' && titulo.back() == '"')
+                {
+                    disco.setCantorBanda(cantorBanda);
+                    break;
+                }
+                else
+                {
+                    std::cout << "Cantor ou banda inválido. Por favor, siga as instruções." << std::endl;
+                }
+            }
+
             std::cout << "Qual o numero de musicas do disco? (Use apenas numeros inteiros)" << std::endl;
-            if (std::cin >> numeroMusicas)
-            {
-                break;
-            }
-            else
-            {
-                std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
-                // Limpar o estado de erro e o buffer de entrada
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
-        disco.setNumeroMusicas(numeroMusicas);
-    }
+            std::cin >> numeroMusicas;
 
-    else if (tipo == "fita de video")
-    {
-        while (true)
+            while (true)
+            {
+                if (std::cin.fail())
+                {
+                    std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
+                    // Limpar o estado de erro e o buffer de entrada
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        if (tipo == "fita de video")
         {
             std::cout << "A fita eh rebobinada? (As opções são 1 para sim ou 0 para nao)" << std::endl;
-            if (std::cin >> rebobinado)
+            std::cin >> rebobinado;
+            while (true)
             {
-                break;
+                if (rebobinado == 1 || rebobinado == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
+                    // Limpar o estado de erro e o buffer de entrada
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+                }
             }
-            else
+            fitaVideo.setRebobinado(rebobinado);
+        }
+
+        std::cout << "Qual a quantidade total da midia? "
+                  << "Use apenas numeros inteiros!" << std::endl;
+        std::cin >> quantidadeTotal;
+        while (true)
+        {
+            if (std::cin.fail())
             {
                 std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
                 // Limpar o estado de erro e o buffer de entrada
                 std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+            }
+            else
+            {
+                break;
             }
         }
-        fitaVideo.setRebobinado(rebobinado);
+        midia.setQuantidadeTotal(quantidadeTotal);
+
+        std::cout << "Qual a quantidade disponivel da midia? "
+                  << "Use apenas numeros inteiros!" << std::endl;
+        std::cin >> quantidadeDisponivel;
+        while (true)
+        {
+            if (std::cin.fail())
+            {
+                std::cout << "Entrada invalida. Por favor, insira novamente." << std::endl;
+                // Limpar o estado de erro e o buffer de entrada
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '/n');
+            }
+            else
+            {
+                break;
+            }
+        }
+        midia.setQuantidadeDisponivel(quantidadeDisponivel);
+
+        std::cout << "Mídia cadastrada com sucesso!" << std::endl;
+        return "Mídia cadastrada com sucesso!";
     }
-
-    std::cout << "Qual a quantidade total da midia? "
-              << "Use apenas numeros inteiros!" << std::endl;
-    std::cin >> quantidadeTotal;
-    midia.setQuantidadeTotal(quantidadeTotal);
-
-    std::cout << "Qual a quantidade disponivel da midia? "
-              << "Use apenas numeros inteiros!" << std::endl;
-    std::cin >> quantidadeDisponivel;
-    midia.setQuantidadeDisponivel(quantidadeDisponivel);
-
-    std::cout << "Mídia cadastrada com sucesso!" << std::endl;
-    return "Mídia cadastrada com sucesso!";
 }
-
 void excluirMidia(int codigo)
 {
-    // Encontrar a mídia com base no código
+// Encontrar a mídia com base no código
     std::vector<Midia> midia;
 
     auto it = std::find_if(midia.begin(), midia.end(), [codigo](const Midia &m)
@@ -195,4 +287,4 @@ void excluirMidia(int codigo)
     {
         std::cout << "Mídia não encontrada." << std::endl;
     }
-}
+    }
--- cadastro_de_midia/dvd.cpp
+++ cadastro_de_midia/dvd.cpp
@@ -6,5 +6,6 @@ DVD::DVD(const std::string& _titulo, int _codigo, const std::string& _genero, in
 
 void DVD::exibirInfo() const {
     Midia::exibirInfo();
-    std::cout << "Categoria: " << categoria << std:: endl; //grupo
+    std::cout << "Categoria: " << categoria << std:: endl;
+    std::cout << "Grupo: " << grupo << std:: endl;
 }
